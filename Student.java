76d41f2662e5658318c2d1a2ebee19994c19b003
Roed <Geofery10@hotmail.com>
2021-04-14 13:02:58 +0200

    Student m. constructor

---
+++ Student.java
@@ -0,0 +1,16 @@
+package OnsdagsOpgaveUge15;
+
+public class Student {
+  private String name;
+  private int[] grade = new int[7];
+  private int[] grades = {-3, 00, 02, 4, 7, 10, 12};
+
+  public Student(String name, int grade[], int [] grades){
+    this.name = name;
+    this.grade = grade;
+    this.grades = grades;
+  }
+
+  
+
+}
