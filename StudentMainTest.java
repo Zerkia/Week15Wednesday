126c03f4f0d9159efe2f87f0ecde267cff453567
Roed <Geofery10@hotmail.com>
2021-04-16 14:57:02 +0200

    Last minute changes to Unittest

--- StudentMainTest.java
+++ StudentMainTest.java
@@ -9,33 +9,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class StudentMainTest {
 
   @Test
-  public void giveGradesPartialTest(){
-    int[] expected = new int[8];
-    expected[0] = 10;
-    expected[1] = 4;
-    expected[2] = 7;
-    expected[3] = 2;
-    expected[4] = 12;
-    expected[5] = 10;
-    expected[6] = 4;
-    expected[7] = 10;
-    Student student = new Student("jens", expected);
-
-    int[] act = new int[8];
-    act[0] = 10;
-    act[1] = 4;
-    act[2] = 7;
-    act[3] = 2;
-    act[4] = 12;
-    act[5] = 10;
-    act[6] = 4;
-    act[7] = 10;
-
-    assertArrayEquals(expected, act);
-
-  }
-
-  @Test
   public void mergeGrades(){
     //Arrange
     StudentMain studentMain = new StudentMain();
@@ -61,9 +34,6 @@ class StudentMainTest {
   public void convertToRealGrade(double totalGrade, double expected) {
     StudentMain studentMain = new StudentMain();
     double actual = studentMain.convertToRealGrade(totalGrade);
-    //double expected = Double.parseDouble(expected1);
-    //actual = (int) actual;
-    //expected = (int) expected;
     assertEquals(expected, actual);
   }
 
